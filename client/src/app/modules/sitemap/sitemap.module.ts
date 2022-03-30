4979d4c139c57741a0b80dffdc603e6999ea486e
Sebastian Roßberger <duckberger@gmail.com>
2022-03-30 20:32:20 +0200

    Added sitemap (#6)

---
+++ client/src/app/modules/sitemap/sitemap.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SitemapComponent } from './sitemap.component';
+
+
+
+@NgModule({
+  declarations: [
+    SitemapComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SitemapModule { }
