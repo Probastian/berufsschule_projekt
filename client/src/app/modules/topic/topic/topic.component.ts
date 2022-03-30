80109f29c0986ecdaeccde9ca401b83087f1fabe
Sebastian Roßberger <sebastian.rossberger@consol.de>
2022-03-30 13:39:45 +0200

    Added label to new post in topic

--- client/src/app/modules/topic/topic/topic.component.ts
+++ client/src/app/modules/topic/topic/topic.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'topic',
@@ -6,10 +6,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./topic.component.sass']
 })
 export class TopicComponent implements OnInit {
+  public availableLabels: string[];
 
-  constructor() { }
+  @ViewChild('autocomplete')
+  public autocomplete: ElementRef | undefined;
 
-  ngOnInit(): void {
+  @ViewChild('label')
+  public label: ElementRef | undefined;
+
+  constructor() {
+    this.availableLabels = this.getAvailableLabels();
   }
 
+  ngOnInit(): void { }
+
+  ngAfterViewInit(): void { }
+
+  getAvailableLabels(): string[] {
+    return [
+      "label1",
+      "label2",
+      "test1",
+      "kack"
+    ];
+  }
+
+  addLabel(element: any): void {
+    element = element.target;
+    if (element.value == "") {
+      return
+    }
+    if (!this.isLabelValid(element.value)) {
+      alert("Not a valid label!")
+      return
+    }
+    //<li class="labeltag" (click)="removeLabel($event)">Label</li>
+    let newElem = document.createElement("li")
+    newElem.classList.add("labeltag")
+    newElem.innerHTML = element.value;
+    element.parentElement.insertBefore(newElem, element);
+    element.value = "";
+  }
+
+  removeLabel(element: any): void {
+    element.target.remove();
+  }
+
+  isLabelValid(labelname: string): boolean {
+    return this.availableLabels.includes(labelname);
+  }
+
+  @HostListener('click', ['$event']) onClick(event:any) {
+    if (event.target.classList.contains("labeltag")) {
+      this.removeLabel(event);
+    }
+  }
 }
